a53d6bd4817edecd75c20867cae613a9ca811c83
yunlingfly <508821881@qq.com>
2020-12-15 13:55:58 +0800

    [DEL] mybatis

--- src/main/java/cn/yunlingfly/springbootflywaydb/SpringbootFlywaydbApplication.java
+++ src/main/java/cn/yunlingfly/springbootflywaydb/SpringbootFlywaydbApplication.java
@@ -1,6 +1,5 @@
 package cn.yunlingfly.springbootflywaydb;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
--- src/main/java/cn/yunlingfly/springbootflywaydb/mapper/PersonMapper.java
+++
@@ -1,9 +0,0 @@
-package cn.yunlingfly.springbootflywaydb.mapper;
-
-import cn.yunlingfly.springbootflywaydb.entity.PersonEntity;
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import org.springframework.stereotype.Component;
-
-@Component
-public interface PersonMapper extends BaseMapper<PersonEntity> {
-}
